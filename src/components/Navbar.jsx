8fcefec2b78b3e93378259690a8ec722e3dd975e
linhpha <linhpha@net.chalmers.se>
2025-04-07 13:12:28 +0200

    #2-Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      NavBar
-    </div>
+    <header className='header'>
+
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white 
+      items-center justify-center flex font-bold shadow-sm">
+        <p className="blue-gradient_text">AH</p>
+      </NavLink>
+      <nav className="f1ex text-lg gap-7 font-medium" >
+        <NavLink to="/about" className={({ isActive }) => isActive ?
+          ' text-blue-500' : ' text-black'}>
+          About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ?
+          ' text-blue-500' : ' text-black'}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
   )
 }
 
