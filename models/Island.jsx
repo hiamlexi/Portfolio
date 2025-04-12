901daf3116c358ef20066c6b430a68deae25566d
hiamlexi <linhpha0102@gmail.com>
2025-04-13 00:59:56 +0200

    add island for homepage

---
+++ models/Island.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Island = () => {
+  return (
+    <div>Island</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Loader } from '../components/Loader'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
