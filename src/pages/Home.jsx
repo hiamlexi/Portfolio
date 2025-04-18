c17e3b2826072ac50921a4b3118c7b650a5e894b
hiamlexi <linhpha0102@gmail.com>
2025-04-19 01:06:14 +0200

    #small fix sky and rotation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
+import { Sky } from '../models/Sky';
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -24,11 +25,12 @@ const Home = () => {
     <section className="w-full h-screen relative">
       <Canvas className="h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
+          <directionalLight position={[10,1,1]} intensity={2} />
+          <ambientLight intensity={0.5} />
           <pointLight />
           <spotLight />
-          <hemisphereLight />
+          <hemisphereLight skyColor="#b1e1ff" intensity={1} />
+          <Sky/>
           <Island position={islandPosition} rotation={islandRotation} scale={islandScale} />
         </Suspense>
       </Canvas>
