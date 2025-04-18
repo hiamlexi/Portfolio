9221a1298daaa60fe4e016de8c44a93cf76c0454
hiamlexi <linhpha0102@gmail.com>
2025-04-19 01:43:54 +0200

    #5 add bird and plane

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
-import { Suspense } from 'react';
+import { useState,Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
-import { Sky } from '../models/Sky';
+import  {Sky}  from '../models/Sky';
+import  Bird from '../models/Bird';
+import Plane  from '../models/Plane';
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
   const adjustIslandForScreenSize = () => {
+ 
     let screenScale = null;
     let screenPostion = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
 
-    if (window.innerWidth < 768) {
+     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
@@ -23,13 +27,16 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
-      <Canvas className="h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
+      <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+ camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight position={[10,1,1]} intensity={2} />
           <ambientLight intensity={0.5} />
           <pointLight />
           <spotLight />
           <hemisphereLight skyColor="#b1e1ff" intensity={1} />
+          <Plane/>
+          <Bird/>
           <Sky/>
           <Island position={islandPosition} rotation={islandRotation} scale={islandScale} />
         </Suspense>
