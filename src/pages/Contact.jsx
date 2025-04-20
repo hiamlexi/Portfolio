d12f9b5f1d8b206268a45c22c3bd87e809c9caeb
hiamlexi <linhpha0102@gmail.com>
2025-04-20 23:37:18 +0200

    #structure the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Contact = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-export default Contact
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleFocus = e => {};
+  const handleBlur = e => {};
+  const handleSubmit =()  => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Hi"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="your@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Leave a message
+            <textarea
+              name="message"
+              rows={4}
+              className="input"
+              placeholder="Leave your msg here for me"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
