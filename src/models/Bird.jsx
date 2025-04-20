36bd0e335740b15c34afa542df817e7a558e1bca
hiamlexi <linhpha0102@gmail.com>
2025-04-20 22:22:15 +0200

    #update bord moving

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
-import birdScene from '../assets/3d/bird.glb' // ✅ Make sure extension is correct
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF, useAnimations } from '@react-three/drei';
+import birdScene from '../assets/3d/bird.glb' 
 
-const Bird = (props) => {
-  const { scene, animations } = useGLTF(birdScene)
+const Bird = () => {
+  const birdRef = useRef();
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+  
+  useEffect(() => {
+    actions['Take 001']?.play();
+  }, [actions]);
+  
+  useFrame(({ clock, camera }) => {
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
 
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      birdRef.current.rotation.y = 0;
+    }
+
+    if (birdRef.current.rotation.y === 0) {
+      // Moving forward
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      // Moving backward
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
   return (
-    <primitive object={scene} {...props} />
+    <mesh position={[-5, 2, 1]} ref={birdRef}
+    scale={[0.003, 0.003, 0.003]}>
+    <primitive object={scene} />
+  </mesh>
   )
 }
 
