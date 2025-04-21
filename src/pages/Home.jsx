1401ee9f50aac80a2892186a6fa46b67fcee8970
hiamlexi <linhpha0102@gmail.com>
2025-04-21 17:15:31 +0200

    #refine homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,39 @@
-import { useState, Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import Loader from '../components/Loader';
-import HomeInfo from '../components/Homeinfo';
-import Island from '../models/Island';
-import { Sky } from '../models/Sky';
-import Bird from '../models/Bird';
-import Plane from '../models/Plane';
+import { Suspense, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import HomeInfo from "../components/Homeinfo";
+import Island from "../models/Island";
+import { Sky } from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+import sakura from "../assets/sakura.mp3";
+import { soundoff, soundon } from "../assets/icons";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
+
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
+
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  const toggleMusic = async () => {
+    const audio = audioRef.current;
+
+    if (!isPlayingMusic) {
+      try {
+        await audio.play();
+        setIsPlayingMusic(true);
+      } catch (err) {
+        console.warn("Autoplay blocked:", err);
+      }
+    } else {
+      audio.pause();
+      setIsPlayingMusic(false);
+    }
+  };
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -27,7 +51,7 @@ const Home = () => {
 
   const adjustPlaneForScreenSize = () => {
     let screenScale, screenPosition;
-  
+
     if (window.innerWidth < 768) {
       screenScale = [1.5, 1.5, 1.5];
       screenPosition = [0, -1.5, 0];
@@ -35,23 +59,23 @@ const Home = () => {
       screenScale = [3, 3, 3];
       screenPosition = [0, -2, -2];
     }
-  
+
     return [screenScale, screenPosition];
   };
-  
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-  const [planeScale, planePosition, planeRotation] = adjustPlaneForScreenSize();
 
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
-    <section className='w-full h-screen relative'>
-    <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-      {currentStage && <HomeInfo currentStage={currentStage} />}
-    </div>
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
 
       <Canvas
         className={`w-full h-screen bg-transparent ${
-          isRotating ? 'cursor-grabbing' : 'cursor-grab'
+          isRotating ? "cursor-grabbing" : "cursor-grab"
         }`}
         camera={{ near: 0.1, far: 1000 }}
       >
@@ -61,14 +85,15 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight skyColor="#b1e1ff" intensity={1} />
-          <Plane 
-            isRotating ={isRotating}
-            planeScale = {planeScale}
-            planePosition = {planePosition}
-            rotation = {[0,20,0]}
+
+          <Plane
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
           />
           <Bird />
-          <Sky isRotating={isRotating}/>
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
@@ -79,6 +104,15 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
+
+      <div className="absolute bottom-2 left-2">
+        <img
+          src={isPlayingMusic ? soundon : soundoff}
+          alt="jukebox"
+          onClick={toggleMusic}
+          className="w-10 h-10 cursor-pointer object-contain"
+        />
+      </div>
     </section>
   );
 };
